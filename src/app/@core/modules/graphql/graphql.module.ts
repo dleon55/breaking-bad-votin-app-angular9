660d614631693be3499d46c03cdacf265b6698d7
dleon <david.gomez@diputados.gob.mx>
2020-10-31 16:02:29 -0600

    Inicial

---
+++ src/app/@core/modules/graphql/graphql.module.ts
@@ -0,0 +1,42 @@
+import { NgModule } from '@angular/core';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { ApolloModule, Apollo } from 'apollo-angular';
+import { HttpLinkModule, HttpLink } from 'apollo-angular-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { WebSocketLink } from 'apollo-link-ws';
+import { getMainDefinition } from 'apollo-utilities';
+import { split } from 'apollo-link';
+@NgModule({
+  exports: [HttpClientModule, ApolloModule, HttpLinkModule],
+})
+export class GraphqlModule {
+  constructor(apollo: Apollo, httpClient: HttpClient) {
+    //Configurar url principal con el link
+    const httpLink = new HttpLink(httpClient).create({
+      uri: "http://localhost:5012/graphql",
+    });
+
+    //Configurar ws con el link
+    const subscriptionLink = new WebSocketLink({
+      uri: "ws://localhost:5012/graphql",
+      options: {
+        reconnect: true,
+      },
+    });
+    //Unir dos conexiones
+    const link = split(
+      ({ query }) => {
+        const { kind, operation }:any = getMainDefinition(query);
+        return kind === "OperationDefinition" && operation === "subscription";
+      },
+      subscriptionLink,
+      httpLink
+    );
+
+    //Crear conexión
+    apollo.create({
+      link,
+      cache: new InMemoryCache()
+    });
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { GraphqlModule } from './@core/modules/graphql/graphql.module';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    GraphqlModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
